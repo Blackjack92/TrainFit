0acb94c70c218bea6b2777105cfafae7b2004963
Kevin <kevin.wallis@hotmail.com>
2015-04-24 20:32:53 +0200

    Init commit

---
+++ TrainFit.Application/ViewModels/ViewModelsBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainFit.Platform;
+
+namespace TrainFit.Application.ViewModels
+{
+    public abstract class ViewModelsBase : NotifyPropertyChangedBase
+    {
+    }
+}
