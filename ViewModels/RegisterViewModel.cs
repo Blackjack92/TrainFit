6096edfecbd3ebf1e69e5c2425e337a97b3b3060
Ferhat <ferhat_satir@hotmail.com>
2015-05-06 21:22:20 +0200

    added TextBlock´s TextBoxes and Buttons to fill the fields in Register Form added button to the Main to Navigate to the Register Form

--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -13,28 +13,55 @@ namespace TrainFit.ViewModels
     public class RegisterViewModel : ViewModel
     {
         #region fields
-        private string text;
+        private string firstname, lastname, emailaddress, password;
         private INavigationService navigationService;
         #endregion
 
         #region properties
-        public string RegisterText
+        public string FirstName
         {
-            get { return text; }
+            get { return firstname; }
             set 
             {
-                SetProperty(ref text, value);
+                SetProperty(ref firstname, value);
             }
         }
 
-        public ICommand ChangeTextCommand { get; private set; }
+        public string LastName
+        {
+            get { return lastname; }
+            set
+            {
+                SetProperty(ref lastname, value);
+            }
+        }
+
+        public string EmailAddress
+        {
+            get { return emailaddress; }
+            set
+            {
+                SetProperty(ref emailaddress, value);
+            }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                SetProperty(ref password, value);
+            }
+        }
+
+        public ICommand ChangeTextFieldsCommand { get; private set; }
         #endregion
 
         #region ctor
         public RegisterViewModel(INavigationService navigationService)
         {
             this.navigationService = navigationService;
-            ChangeTextCommand = new DelegateCommand(ChangeText);
+            ChangeTextFieldsCommand = new DelegateCommand(ChangeText);
         }
         #endregion
 
@@ -42,7 +69,10 @@ namespace TrainFit.ViewModels
         private void ChangeText()
         {
             var random = new Random();
-            RegisterText = random.Next(1000, 10000).ToString();
+            FirstName = random.Next(100, 199).ToString();
+            LastName = random.Next(200, 299).ToString();
+            EmailAddress = random.Next(300, 399).ToString();
+            Password = random.Next(400, 500).ToString();
 
             navigationService.Navigate("Main", null);
         }
