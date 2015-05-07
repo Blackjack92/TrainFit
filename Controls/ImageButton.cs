7df6ce517ecc8cadb9b69b7ea996145e791ad326
Kevin <kevin.wallis@hotmail.com>
2015-05-07 19:01:18 +0200

    Improved image button

--- Controls/ImageButton.cs
+++ Controls/ImageButton.cs
@@ -15,25 +15,28 @@ namespace TrainFit.Controls
         public BitmapImage ImageSource
         {
             get { return (BitmapImage)GetValue(ImageSourceProperty); }
-            set { SetValue(ImageSourceProperty, value); }
+            set { SetValue(ImageSourceProperty, (BitmapImage)value); }
         }
 
         // Using a DependencyProperty as the backing store for ImageSource.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty ImageSourceProperty =
-            DependencyProperty.Register("ImageSource", typeof(BitmapImage), typeof(ImageButton), new PropertyMetadata(null, OnImageSourceChanged));
+            DependencyProperty.Register("ImageSource", typeof(BitmapImage), typeof(ImageButton), null);
+
+        public BitmapImage ImageSourcePressed
+        {
+            get { return (BitmapImage)GetValue(ImageSourcePressedProperty); }
+            set { SetValue(ImageSourcePressedProperty, (BitmapImage)value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ImageSource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageSourcePressedProperty =
+            DependencyProperty.Register("ImageSourcePressed", typeof(BitmapImage), typeof(ImageButton), null);
         #endregion
 
         #region ctor
         public ImageButton()
         {
             this.DefaultStyleKey = typeof(ImageButton);
-        }
-        #endregion
-
-        #region methods
-        private static void OnImageSourceChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
-        {
-
         }
         #endregion
     }
