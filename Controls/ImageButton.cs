7232c05ee1b0beda95bc92b56a248eecba5024f0
Kevin <kevin.wallis@hotmail.com>
2015-05-07 22:21:41 +0200

    Updated sidebar icons and selection behavior

--- Controls/ImageButton.cs
+++ Controls/ImageButton.cs
@@ -12,25 +12,19 @@ namespace TrainFit.Controls
     public class ImageButton : Button
     {
         #region dependency properties
-        public BitmapImage ImageSource
+
+
+        public ControlTemplate IconTemplate
         {
-            get { return (BitmapImage)GetValue(ImageSourceProperty); }
-            set { SetValue(ImageSourceProperty, (BitmapImage)value); }
+            get { return (ControlTemplate)GetValue(IconTemplateProperty); }
+            set { SetValue(IconTemplateProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for ImageSource.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ImageSourceProperty =
-            DependencyProperty.Register("ImageSource", typeof(BitmapImage), typeof(ImageButton), null);
+        // Using a DependencyProperty as the backing store for IconTemplate.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconTemplateProperty =
+            DependencyProperty.Register("IconTemplate", typeof(ControlTemplate), typeof(ImageButton), null);
 
-        public BitmapImage ImageSourcePressed
-        {
-            get { return (BitmapImage)GetValue(ImageSourcePressedProperty); }
-            set { SetValue(ImageSourcePressedProperty, (BitmapImage)value); }
-        }
 
-        // Using a DependencyProperty as the backing store for ImageSource.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ImageSourcePressedProperty =
-            DependencyProperty.Register("ImageSourcePressed", typeof(BitmapImage), typeof(ImageButton), null);
         #endregion
 
         #region ctor
