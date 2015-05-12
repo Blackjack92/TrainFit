3fac37e76f3ca6f9b349b3f4f7da950974d16b1f
Kevin <kevin.wallis@hotmail.com>
2015-05-12 22:18:36 +0200

    Added IIdentifiable interface for the guid and primary key

---
+++ Models/IIdentifiable.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Practices.Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainFit.Models
+{
+    public interface IIdentifiable
+    {
+        #region properties
+        Guid Id { get; set; }
+        #endregion
+    }
+}
