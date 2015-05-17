dcb6530c78d31b02f54f767288ab42e97d06cede
Kevin <kevin.wallis@hotmail.com>
2015-05-17 15:32:13 +0200

    Added timer service e.g. to save a round time

---
+++ Services/ITimerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainFit.Services
+{
+    public interface ITimerService
+    {
+        bool StartTimer { get; set; }
+        bool StopTimer { get; set; }
+        TimeSpan CurrentTime { get; set; }
+    }
+}
--- Services/TimeService.cs
+++ Services/TimeService.cs
@@ -3,46 +3,65 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading;
+using TrainFit.Utils;
 using Windows.UI.Xaml;
 
 namespace TrainFit.Services
 {
-    public class TimeService : BindableBase
+    public static class TimerServiceNames
+    {
+        public static readonly string StartTimer = StaticReflection.GetMemberName<TimerService>(service => service.StartTimer);
+        public static readonly string StopTimer = StaticReflection.GetMemberName<TimerService>(service => service.StopTimer);
 
+    }
+
+    public class TimerService : BindableBase, ITimerService
     {
         #region fields
         private DispatcherTimer timer;
         private bool startTimer;
         private bool stopTimer;
-        private TimeSpan time;
+        private TimeSpan currentTime;
         #endregion
 
         #region properties
         public bool StartTimer { get { return startTimer; } set { SetProperty(ref startTimer, value); } }
         public bool StopTimer { get { return stopTimer; } set { SetProperty(ref stopTimer, value); } }
-        public TimeSpan Time { get { return time; } set { SetProperty(ref time, value); } }
+        public TimeSpan CurrentTime { get { return currentTime; } set { SetProperty(ref currentTime, value); } }
         #endregion
 
         #region ctor
-        public TimeService()
+        public TimerService()
         {
+            CurrentTime = new TimeSpan();
             timer = new DispatcherTimer();
             timer.Tick += OnTimerTick;
             PropertyChanged += OnPropertyChanged;
         }
 
-        void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            // TODO: Handle property changed e.g. enable/disable timer
-            throw new NotImplementedException();
+            if (e.PropertyName == TimerServiceNames.StartTimer && StartTimer)
+            {
+                CurrentTime = new TimeSpan();
+                timer.Start();
+                StopTimer = false;
+            }
+            else if (e.PropertyName == TimerServiceNames.StopTimer && StopTimer)
+            {
+                timer.Stop();
+                StartTimer = false;
+            }
         }
 
         private void OnTimerTick(object sender, object e)
         {
             // TODO: Handle timer tick e.g. with a callback
-            throw new NotImplementedException();
+            CurrentTime += timer.Interval;
         }
         #endregion
     }
