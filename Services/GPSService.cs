defd972d9d156887e69cc3b7dab2860f5f046647
Kevin <kevin.wallis@hotmail.com>
2015-05-17 15:30:35 +0200

    #15 - Added gps service

---
+++ Services/GPSService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Practices.Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace TrainFit.Services
+{
+    public class GPSService : BindableBase
+    {
+        #region fields
+        private Geolocator geolocator;
+        #endregion
+
+        #region properties
+        #endregion
+
+        #region ctor
+        public GPSService()
+        {
+            geolocator = new Geolocator();
+            geolocator.DesiredAccuracyInMeters = 5;
+        }
+        #endregion
+
+        #region methods
+        // TODO: Remove this method if not needed
+        //public async Geoposition GetPosition()
+        //{
+        //    var cancellationTokenSource = new CancellationTokenSource();
+        //    var token = cancellationTokenSource.Token;
+
+        //    // Carry out the operation 
+        //    return await geolocator.GetGeopositionAsync().AsTask(token); 
+        //}
+        #endregion
+    }
+}
---
+++ Services/TimeService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Practices.Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Windows.UI.Xaml;
+
+namespace TrainFit.Services
+{
+    public class TimeService : BindableBase
+
+    {
+        #region fields
+        private DispatcherTimer timer;
+        private bool startTimer;
+        private bool stopTimer;
+        private TimeSpan time;
+        #endregion
+
+        #region properties
+        public bool StartTimer { get { return startTimer; } set { SetProperty(ref startTimer, value); } }
+        public bool StopTimer { get { return stopTimer; } set { SetProperty(ref stopTimer, value); } }
+        public TimeSpan Time { get { return time; } set { SetProperty(ref time, value); } }
+        #endregion
+
+        #region ctor
+        public TimeService()
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += OnTimerTick;
+            PropertyChanged += OnPropertyChanged;
+        }
+
+        void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            // TODO: Handle property changed e.g. enable/disable timer
+            throw new NotImplementedException();
+        }
+
+        private void OnTimerTick(object sender, object e)
+        {
+            // TODO: Handle timer tick e.g. with a callback
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
