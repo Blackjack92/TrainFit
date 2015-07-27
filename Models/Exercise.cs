a48aa452e25013755aa1d0faf48a41e8656d8fd8
Kevin <kevin.wallis@hotmail.com>
2015-07-27 23:49:35 +0200

    Added XmlServiceTest. Added XmlIgnoreAttribute. Find a better solution. Reviewed DatabaseServiceTest.

--- Models/Exercise.cs
+++ Models/Exercise.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace TrainFit.Models
 {
@@ -22,9 +23,16 @@ namespace TrainFit.Models
         #region properties
         public string Name { get { return name; } set { SetProperty(ref name, value); } }
         public Difficulty Difficulty { get { return difficulty; } set { SetProperty(ref difficulty, value); } }
+        [XmlIgnoreAttribute]
         public Uri Url { get { return url; } set { SetProperty(ref url, value); } }
+        [XmlIgnoreAttribute]
         public Uri ImageSource { get { return imageSource; } set { SetProperty(ref imageSource, value); } }
         public string Description { get { return description; } set { SetProperty(ref description, value); } }
+        #endregion
+
+        #region ctor
+        public Exercise()
+        { }
         #endregion
     }
 }
