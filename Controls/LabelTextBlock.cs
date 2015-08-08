91016bd90a1b1bb6b14a853b0dc7d0c3c4b4c8b6
Kevin <kevin.wallis@hotmail.com>
2015-08-08 20:06:38 +0200

    Added a label textblock control.

---
+++ Controls/LabelTextBlock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace TrainFit.Controls
+{
+    // Inherit from TextBox because TextBlock is sealed
+    public class LabelTextBlock : TextBox
+    {
+        #region dependency properties
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Label.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register("Label", typeof(string), typeof(LabelTextBlock), null);
+        #endregion
+
+        #region ctor
+        public LabelTextBlock()
+        {
+            this.DefaultStyleKey = typeof(TextBlock);
+        }
+        #endregion
+    }
+}
