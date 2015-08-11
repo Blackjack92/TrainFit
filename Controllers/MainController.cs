be6ee0bd0601d305cd3e11055d65cbe42ef97651
Kevin <kevin.wallis@hotmail.com>
2015-08-11 20:15:58 +0200

    Added small logic to the controller, this logic we have to discuss.

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -1,6 +1,18 @@
-﻿namespace TrainFit.Controllers
+﻿using Microsoft.Practices.Prism.Mvvm.Interfaces;
+
+namespace TrainFit.Controllers
 {
     public class MainController
     {
+        #region properties
+        public INavigationService NavigationService { get; private set; }
+        #endregion
+
+        #region ctor
+        public MainController(INavigationService navigationService)
+        {
+            NavigationService = navigationService;
+        }
+        #endregion
     }
 }
