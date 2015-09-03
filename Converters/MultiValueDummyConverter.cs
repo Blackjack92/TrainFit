ab8287858d751bc09d1e4627853cbd12a2efae09
Kevin <kevin.wallis@hotmail.com>
2015-09-03 21:43:35 +0200

    Still some improvements left.

---
+++ Converters/MultiValueDummyConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainFit.DataModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace TrainFit.Converters
+{
+    public class MultiValueDummyConverter : DependencyObject, IValueConverter
+    {
+        public object SelectedItem
+        {
+            get { return GetValue(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedItem.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register("SelectedItem", typeof(object), typeof(MultiValueDummyConverter), new PropertyMetadata(0));
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
