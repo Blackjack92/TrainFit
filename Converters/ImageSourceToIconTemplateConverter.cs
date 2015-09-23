a1154ff7408bf0072fb184f12739d7c97469a88d
Kevin <kevin.wallis@hotmail.com>
2015-09-23 13:27:18 +0200

    Started updating of the images from the exercises page.

---
+++ Converters/ImageSourceToIconTemplateConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace TrainFit.Converters
+{
+    public class ImageSourceToIconTemplateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return Application.Current.Resources["Icon.Test"];
+            //throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
